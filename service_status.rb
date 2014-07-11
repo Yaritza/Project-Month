0bcf3904749648f2a179d6ece75651ef80b77585
Yaritza <saturn540@gmail.com>
2014-07-10 21:06:34 -0400

    checkout the html encode and build from there

--- service_status.rb
+++ service_status.rb
@@ -43,24 +43,70 @@ class Service
 	end
 
 	def service_hash
+		#only pairs train lines with status. 
+		#Will be incorporating  descriptive_stautus later.
 		output.each do |subway_status_pair|
 			@subway_hash[subway_status_pair[0]] = subway_status_pair[1]
 		end
 		 @subway_hash 
 	end
 	def descriptive_status
-		@doc.xpath("//text").collect do  |tag_contents|
-			text =  tag_contents.text
-			@description << text
-		end
-			@description
+
+		################
+		# @doc.xpath("//line").map do  |line|
+		# 	train_name =  line.at_css("name").text
+		# 	train_status = line.at_css("status").text
+		# 	descriptive_status = nil
+		# 	binding.pry
+		# 	#descriptive_status = line.at_css("TitleDelay") #||= line.at_css("TitlePlannedWork plannedWorkDetailLink") ||= line.at_css("TitleServiceChange")
+		# 	@description << [train_name, train_status] 
+		# end
+		# 	@description[0..9]
 		
+		# def descriptive_status
+		# binding.pry
+		#####################
+
+		@doc.xpath("//line").collect do |line|
+			puts "******************************"
+			train_name = line.at_css("/name").text
+			train_status = line.at_css("status").text
+			# binding.pry if train_name == "G"
+			descriptive_status = line.children[5].children[0]
+			if train_status == "GOOD SERVICE"
+				puts "Hooray! Good service  for the #{train_name} today!"
+			else 
+				puts "Give yourself extra time. There are issues with your line."
+				puts "Here is the description: "
+					 description = descriptive_status.to_s
+					 @description = description
+					 #Our description live between this gibberish &lt;br/&gt;&lt;br/&gt;
+			end 
+		end 
+		@description = @description.gsub("&lt;br/&gt;&lt;br/&gt;", "XXX")
+		@description = @description.gsub(/(XXX).+(XXX)/)
 	end
 end
 
-service = Service.new
+
+html = <<-HTML
+
+&lt;span class="TitleServiceChange" &gt;Sandy Reroute&lt;/span&gt;
+                    &lt;span class="DateStyle"&gt;
+                    &amp;nbsp;Posted:&amp;nbsp;07/10/2014&amp;nbsp; 8:01PM
+                    &lt;/span&gt;&lt;br/&gt;&lt;br/&gt;
+                  &lt;STRONG&gt;[FF]&lt;BR&gt;[R] No trains between Whitehall St and Downtown Brooklyn - Take the [N] instead&lt;BR&gt;Transfer between [N] and [R] trains at Canal St and Atlantic Av-Barclays Ctr&lt;BR&gt;&lt;/STRONG&gt;Weekdays until Oct 2014
+                &lt;br/&gt;&lt;br/&gt;
+
+HTML
+
+require 'pp'
+
+pp Nokogiri::HTML(html).text
+
+# service = Service.new
 #testing:
 #line_result = service.doc.xpath("//subway").xpath("//line").first
 
 
-binding.pry
+# binding.pry
