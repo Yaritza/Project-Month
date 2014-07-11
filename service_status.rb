aa8bfdea0cbee172f72795e411e2cab215e8309a
Christina Cannito <christinacannito@gmail.com>
2014-07-11 14:22:30 -0400

    slight adjustment to method

--- service_status.rb
+++ service_status.rb
@@ -17,42 +17,12 @@ class Service
 	end
 
 	def subway_list
-		#Hard coding  for a test-line.
-		#@trainline = NQR
-		#Subway details are in parent <subway> tag
-		#
-		#<line> <name> contains line names
-		#subway options are 123, 456, 7, ACE, BDFM, G, JZ, L, NQR, S
-		
-		#Following line isolates the subway specific data (first 10 are all subways only)
 			@doc.xpath("//name").collect do |lines|
 				lines = lines.text
 				@lines = lines
 			end
 	end
 
-	def service_list
-		@doc.xpath("//status").collect do |status|
-			 status = status.text
-			 @status = status
-		end
-	end
-
-	def output
-	 subway_list.zip(service_list)[0..9]
-
-	end
-
-	def service_hash
-		output.each do |subway_status_pair|
-			@subway_hash[subway_status_pair[0]] = subway_status_pair[1]
-		end
-		 @subway_hash 
-	end
-
-	def print_out
-		puts @doc
-	end
 
 	def descriptive_status
 		@doc.xpath('//subway//line').each do |line|
@@ -68,7 +38,14 @@ class Service
 			html.css(".plannedWorkDetailLink").each do |detail|
 				puts train_name
 				puts "----------------"
-				puts detail.children.text
+				puts detail.children.text.inspect
+			end
+
+			html.css(".plannedWorkDetail").each do |detail|
+				puts train_name
+				puts "----------------"
+				puts "THIS IS THE DETAIL:"
+				puts detail.children.text.inspect
 			end
 
 			if train_status == "GOOD SERVICE"
